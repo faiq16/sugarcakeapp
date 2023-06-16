5fbdb4655a0a97d732a6b5bb4ee82791c66e788c
Soviaviay <soviaviay04@gmail.com>
2023-06-16 16:06:45 +0800

    theme

--- lib/home/screen/home_screen.dart
+++ lib/home/screen/home_screen.dart
@@ -3,16 +3,55 @@ import 'package:flutter/material.dart';
 import '../../constants.dart';
 import '../components/body.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _iconBool = false;
+  IconData _iconLight = Icons.wb_sunny;
+  IconData _iconDark = Icons.nights_stay;
+
+  ThemeData _lightTheme = ThemeData(
+    primarySwatch: Colors.amber,
+    brightness: Brightness.light,
+    buttonTheme: const ButtonThemeData(
+      buttonColor: Colors.amberAccent,
+    ),
+  );
+
+  ThemeData _darkTheme = ThemeData(
+    primarySwatch: Colors.red,
+    brightness: Brightness.dark,
+  );
+
+    @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(),
-      body: const Body(),
+    return MaterialApp(
+      title: 'SugarCake',
+      theme: _iconBool ? _darkTheme : _lightTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Sugar Cake"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _iconBool = !_iconBool;
+                });
+              },
+              icon: Icon(_iconBool ? _iconDark : _iconLight),
+            ),
+          ],
+        ),
+        body: const Body(),
+      ),
     );
   }
+
 
   AppBar buildAppBar() {
     Widget profileImage() => Container(
